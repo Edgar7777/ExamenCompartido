bb47bbf253e8ee3d34bf7810c6ad39d6c5b2cb38
Edgar7777 <losthedgar@hotmail.com>
2018-06-03 13:21:49 -0500

    primeng

--- examen1Bim/src/app/app.module.ts
+++ examen1Bim/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+//import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+//import {MenuItem} from 'primeng/api';                 //api
 
 import { AppComponent } from './app.component';
 
